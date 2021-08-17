881bbd83361ea2c7a83f40bd8c8695cf3caa4be0
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-08-17 13:34:10 +0300

    small fix

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 const countdown = () => {
-    const countDate = new Date("August 18, 2021 00:00:00").getTime()
-    const now = new Date().getTime();
-    const gap = countDate - now;
+    let countDate = new Date("August 18, 2021 00:00:00").getTime()
+    let now = new Date().getTime();
+    let difference = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+    let tDay = Math.floor( difference / day);
+    let tHour = Math.floor((difference % day) / hour);
+    let tMinute = Math.floor((difference % hour) / minute);
+    let tSecond = Math.floor((difference % minute) / second);
 
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
-
-    document.querySelector('.day').innerHTML = textDay;
-    document.querySelector(".hour").innerText = textHour;
-    document.querySelector(".minute").innerText = textMinute;
-    document.querySelector(".second").innerText = textSecond;
-
-    console.log(textDay)
+    document.querySelector('.day').innerHTML = tDay;
+    document.querySelector(".hour").innerText = tHour;
+    document.querySelector(".minute").innerText = tMinute;
+    document.querySelector(".second").innerText = tSecond;
 };
-
 
 setInterval(countdown, 1000);
